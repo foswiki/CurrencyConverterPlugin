9e1b301e756ecf7d9dea6309e012f45c49c08bca
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/CurrencyConverterPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/CurrencyConverterPlugin.pm
+++ lib/Foswiki/Plugins/CurrencyConverterPlugin.pm
@@ -19,8 +19,8 @@
 package Foswiki::Plugins::CurrencyConverterPlugin;
 
 use strict;
-require Foswiki::Func;    # The plugins API
-require Foswiki::Plugins; # For the API version
+require Foswiki::Func;       # The plugins API
+require Foswiki::Plugins;    # For the API version
 
 our $VERSION = '$Rev: 1$';
 
@@ -32,12 +32,12 @@ our $NO_PREFS_IN_TOPIC = 1;
 #=cut
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 2.0 ) {
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
         Foswiki::Func::writeWarning( 'Version mismatch between ',
-                                     __PACKAGE__, ' and Plugins.pm' );
+            __PACKAGE__, ' and Plugins.pm' );
         return 0;
     }
 
@@ -48,115 +48,94 @@ sub initPlugin {
     return 1;
 }
 
-sub _convertCurrency
-{
-    my($session, $params, $theTopic, $theWeb) = @_;
+sub _convertCurrency {
+    my ( $session, $params, $theTopic, $theWeb ) = @_;
 
-#printf STDERR "%i\n", exists($params->{amount});
+    #printf STDERR "%i\n", exists($params->{amount});
 
     my $amount = 1;
-    if ( exists($params->{amount}) )
-    {
-        $amount = defined($params->{amount}) ? $params->{amount} : 0.0;
+    if ( exists( $params->{amount} ) ) {
+        $amount = defined( $params->{amount} ) ? $params->{amount} : 0.0;
     }
     my $from = $params->{from} || "EUR";
-    my $to = $params->{to} || "EUR";
+    my $to   = $params->{to}   || "EUR";
 
     my $date;
-    if ( exists($params->{date}) )
-    {
-	$date = $params->{date};
+    if ( exists( $params->{date} ) ) {
+        $date = $params->{date};
     }
-    else
-    {
-        my @d=(localtime)[5,4,3];
-        $d[0]+=1900;
-        $d[1]+=1;
-        $date = sprintf ("%s-%s-%s",@d);
-    };
+    else {
+        my @d = (localtime)[ 5, 4, 3 ];
+        $d[0] += 1900;
+        $d[1] += 1;
+        $date = sprintf( "%s-%s-%s", @d );
+    }
 
-#printf STDERR "%s, %s, %s, %f\n", $from, $to, $date, $amount;
+    #printf STDERR "%s, %s, %s, %f\n", $from, $to, $date, $amount;
 
     require DBI;
 
-    my $dbname="currency";
-    my $username="guest";
-    my $pw="";
+    my $dbname   = "currency";
+    my $username = "guest";
+    my $pw       = "";
 
-    my $db = DBI->connect("DBI:mysql:$dbname", $username, $pw, { RaiseError => 1 })
-        || print STDERR("Error accessing database: $@");
+    my $db =
+      DBI->connect( "DBI:mysql:$dbname", $username, $pw, { RaiseError => 1 } )
+      || print STDERR("Error accessing database: $@");
 
-    if ( $from eq $to )
-    {
-	return $amount;
+    if ( $from eq $to ) {
+        return $amount;
     }
-    elsif ( $from eq "EUR" )
-    {
-        return $amount*fetch_rate_EUR($db, $to, $date);
+    elsif ( $from eq "EUR" ) {
+        return $amount * fetch_rate_EUR( $db, $to, $date );
     }
-    elsif ( $to eq "EUR" )
-    {
-        return $amount/fetch_rate_EUR($db, $from, $date);
+    elsif ( $to eq "EUR" ) {
+        return $amount / fetch_rate_EUR( $db, $from, $date );
     }
-    else
-    {
-        return
-           $amount
-           *fetch_rate_derived($db, $to, $from, $date);
+    else {
+        return $amount * fetch_rate_derived( $db, $to, $from, $date );
     }
 }
 
-sub fetch_rate_EUR
-{
-    my($db, $to, $date) = @_;
+sub fetch_rate_EUR {
+    my ( $db, $to, $date ) = @_;
 
     my $req;
-    eval
-    {
-        $req=$db->prepare
-        (
-            sprintf
-            (
-                "SELECT rate FROM $to WHERE date <= '%s' ORDER BY date DESC LIMIT 1;",
-                $date
-            )
+    eval {
+        $req = $db->prepare(
+            sprintf(
+"SELECT rate FROM $to WHERE date <= '%s' ORDER BY date DESC LIMIT 1;",
+                $date )
         );
-	$req->execute;
+        $req->execute;
     };
 
-    if ( $@ )
-    {
+    if ($@) {
         print STDERR "Error accessing database: $@\n";
         return;
-    };
- 
+    }
+
     return $req->fetchrow_array();
 }
 
-sub fetch_rate_derived
-{
-    my($db, $to, $from, $date) = @_;
+sub fetch_rate_derived {
+    my ( $db, $to, $from, $date ) = @_;
 
     my $req;
-    eval
-    {
-        $req=$db->prepare
-        (
-            sprintf
-            (
-                "SELECT $to.rate/$from.rate FROM $to,$from WHERE $to.date = $from.date AND $to.date <= '%s' ORDER BY $to.date DESC LIMIT 1;",
-                $date
-            )
+    eval {
+        $req = $db->prepare(
+            sprintf(
+"SELECT $to.rate/$from.rate FROM $to,$from WHERE $to.date = $from.date AND $to.date <= '%s' ORDER BY $to.date DESC LIMIT 1;",
+                $date )
         );
-	$req->execute;
+        $req->execute;
     };
 
-    if ( $@ )
-    {
+    if ($@) {
         print STDERR "Error accessing database: $@\n";
         return;
-    };
- 
+    }
+
     return $req->fetchrow_array();
 }
 
